8225a8a37c0e11766d17128c57543f9b5e5fac72
Kingveio <kingveio.contato@gmail.com>
2025-08-12 17:54:23 -0300

    twitch.py

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -9,10 +9,10 @@ class TwitchCommands(commands.Cog):
 
     @commands.command(name="addtwitch")
     async def add_twitch_user(self, ctx, twitch_username: str):
-        # Adicionando um novo alvo da Twitch à memória.
+        guild_id = ctx.guild.id
         user_id = ctx.author.id
-        self.data_manager.add_user(user_id, twitch_name=twitch_username)
-        await ctx.send(f"Alvo '{twitch_username}' adicionado à lista de vigilância da Twitch. Eu voltarei a verificar.")
+        self.data_manager.add_user(guild_id, user_id, twitch_name=twitch_username)
+        await ctx.send(f"Alvo '{twitch_username}' adicionado à lista de vigilância da Twitch para este servidor. Eu voltarei a verificar.")
 
 def setup(bot):
     bot.add_cog(TwitchCommands(bot))
