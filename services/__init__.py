1fe5fab20c24726b9ebf554dd9b49769fbaf2c05
Kingveio <kingveio.contato@gmail.com>
2025-08-13 17:19:35 -0300

    __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,7 @@
+# Exporta os serviços principais para facilitar imports
+from .discord_service import DiscordService
+from .google_drive import GoogleDriveService
+from .twitch_api import TwitchAPI
+from .youtube_api import YouTubeAPI
+
+__all__ = ['DiscordService', 'GoogleDriveService', 'TwitchAPI', 'YouTubeAPI']
