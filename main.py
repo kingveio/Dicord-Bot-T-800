d5d543a1af2486b9c54ab804b6094832df0504d2
Kingveio <kingveio.contato@gmail.com>
2025-08-13 19:47:24 -0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import discord
-from discord.ext import commands
-from config import Config
-
 class T800Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
@@ -9,13 +5,22 @@ class T800Bot(commands.Bot):
         intents.message_content = True
         
         super().__init__(
-            command_prefix="!",  # Prefixo tradicional
+            command_prefix="!",
             intents=intents,
-            help_command=None  # Remove o help padrão
+            help_command=None
         )
+        
+        # Inicializa o DataManager
+        self.data_manager = DataManager()
+        self.data_manager.bot = self  # Permite acesso ao bot
+        
+        # Inicializa APIs
+        self.twitch_api = TwitchAPI()
+        self.youtube_api = YouTubeAPI()
 
     async def setup_hook(self):
-        # Carrega as extensões
+        await self.data_manager.load()  # Carrega os dados
+        await self.load_extension("cogs.live_monitor")
         await self.load_extension("cogs.live_monitor")
         await self.load_extension("cogs.youtube")
         await self.load_extension("cogs.twitch")
