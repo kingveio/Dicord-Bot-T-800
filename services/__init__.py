216f20a5176dd18b5cac6cf3161b76cf2d2fc813
Kingveio <kingveio.contato@gmail.com>
2025-08-15 17:22:14 -0300

    Delete services directory

--- services/__init__.py
+++
@@ -1,7 +0,0 @@
-# Exporta os serviços principais para facilitar imports
-from .discord_service import DiscordService
-from .google_drive_service import GoogleDriveService
-from .twitch_api import TwitchAPI
-from .youtube_api import YouTubeAPI
-
-__all__ = ['DiscordService', 'GoogleDriveService', 'TwitchAPI', 'YouTubeAPI']
