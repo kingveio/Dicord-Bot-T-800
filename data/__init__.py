0cf4fda036a2d7bab1798d3d1d0d931023ddb00e
Kingveio <kingveio.contato@gmail.com>
2025-08-15 17:22:33 -0300

    Delete data directory

--- data/__init__.py
+++
@@ -1,5 +0,0 @@
-# Exporta as classes principais para facilitar imports
-from .models import GuildData, UserData, GuildConfig
-from .data_manager import DataManager
-
-__all__ = ['GuildData', 'UserData', 'GuildConfig', 'DataManager']
